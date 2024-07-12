8ce6b469724d4d9a1eecba6ade4175422e51318e
ThreeSeaGrass <kiron_dev@hotmail.com>
2024-07-12 22:02:05 +0100

    add button styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 child:border-white child:border-2 child:p-4 child:mb-2">
-      <Link href="/frontend">Frontend</Link>
-      <Link href="/backend">Backend</Link>
-      <Link href="/ux">UX</Link>
-      <Link href="/devops">Devops</Link>
-      <Link href="/domain">Domain</Link>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 child:border-white child:border-2 child:p-4 child:mb-2 child:w-full child:text-center child:md:py-16 child:text-xl">
+      <Link className="active:bg-slate-900" href="/frontend">Frontend</Link>
+      <Link className="active:bg-slate-900" href="/backend">Backend</Link>
+      <Link className="active:bg-slate-900" href="/ux">UX</Link>
+      <Link className="active:bg-slate-900" href="/devops">Devops</Link>
+      <Link className="active:bg-slate-900" href="/domain">Domain</Link>
     </main>
   );
 }
